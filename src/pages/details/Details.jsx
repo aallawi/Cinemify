fc263496cda3d91daf044c8502e67ab69deca594
aallawi <allawi100@yahoo.com>
2024-02-12 23:50:13 +0200

    add (Similar - Recommendation)

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -2,6 +2,9 @@ import { useParams } from "react-router-dom";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
 import TopCast from "./topCast/TopCast";
 import useFetch from "../../hooks/useFetch";
+import VideosSection from "./videosSection/VideosSection";
+import Similar from "./similar/Similar";
+import Recommendation from "./recommendation/Recommendation";
 
 const Details = () => {
   const { mediaType, id } = useParams();
@@ -17,6 +20,9 @@ const Details = () => {
     <div className="bg-primary">
       <DetailsBanner intro={videos?.results?.[0]} crew={credits?.crew} />
       <TopCast cast={credits?.cast} loading={creditsLoading} />
+      <VideosSection videos={videos} loading={videosLoading} />
+      <Similar mediaType={mediaType} id={id} />
+      <Recommendation mediaType={mediaType} id={id} />
     </div>
   );
 };
